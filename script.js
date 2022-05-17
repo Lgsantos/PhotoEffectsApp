375a25aa73d79cb3cd8f3f6fd108b08a7fff24af
Luís Gustavo dos Santos <luis.gustavo.santos@gmail.com>
2022-05-16 22:23:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -16,5 +16,19 @@ function uploadImage(e) {
         }
     }
 }
+
+function change() {
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const data = imageData.data;
+    for (let i = 0; i < data.length; i += 4) {
+        data[i] = 255;
+        data[i+1];
+        data[i+2];
+    }
+    ctx.putImageData(imageData, 0, 0);
+}
+
+document.querySelectorAll("button")[0].addEventListener("click", change);
+
 const imageLoader = document.getElementById("uploader");
 imageLoader.addEventListener("change", uploadImage);
